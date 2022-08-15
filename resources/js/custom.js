1647cf1d202cc68c7c12d406c622b6f4bcc6aec2
okulafebrian <okulafebrian@gmail.com>
2022-08-15 12:16:17 +0700

    2nd

---
+++ resources/js/custom.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    
+    //  Show Modal
+    var myModal = new bootstrap.Modal(document.getElementById('alert'));
+    myModal.show();
+    
+
+});
--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,5 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    return view('home');
+    return view('welcome');
 });
